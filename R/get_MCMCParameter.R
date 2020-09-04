3c68f6f0efecdbca06407b9923eaa84ccb0cad28
RLumSK <sebastian.kreutzer@aber.ac.uk>
2020-09-04 21:15:27 +0200

    Add new function to extrac a parameter from an mcmc object.

---
+++ R/get_MCMCParameter.R
@@ -0,0 +1,62 @@
+#'@title Extracts a parameter from an MCMC list
+#'
+#'@description Short cut to extract a parameter from an MCMC list. If more processing
+#'is wanted, the `'coda'` package can be used
+#'
+#'@param mcmc [coda::mcmc] or [coda::mcmc.list] (**required**) input
+#'
+#'@param parameter [character] (**required**): name of the parameter to be extracted
+#'
+#'@param prob [numeric] (*with default*): probability for the HPD calculation
+#'
+#'@param unlist [logical] (*with default*): if `TRUE` the output is a [matrix]
+#'otherwise a [list]
+#'
+#'@return Returns a matrix with the parameter value or a [list]
+#'
+#'@section Function version: 0.1.0
+#'
+#'@author Sebastian Kreutzer, Geography & Earth Sciences, Aberystwyth University (United Kingdom)
+#'
+#'@seealso [coda::HPDinterval]
+#'
+#'@examples
+#'
+#'##TODO
+#'
+#'@md
+#'@export
+get_MCMCParameter <- function(
+  mcmc,
+  parameter,
+  prob = 0.95,
+  unlist = TRUE
+) {
+  ## set parameters for column
+  ncol <- 1
+  valid_names <- unlist(strsplit(coda::varnames(mcmc),"[",TRUE))
+  valid_names <- unique(valid_names[seq(1, length(valid_names), 2)])
+
+  ##check input
+  if(!parameter[1] %in% valid_names)
+    stop("[get_MCMCParameter()] Invalid parameter name, valid names are: ",
+         paste(valid_names, collapse= ", "), call. = FALSE)
+
+  ## extract parameter
+  l <- mcmc[,grepl(coda::varnames(mcmc), pattern = parameter[1], fixed = TRUE)]
+
+  ##if the list is more complex we have to be do more ... we only test
+  ##the first because it should be ok for the rest
+  if(grepl(coda::varnames(l[[1]])[1], pattern = ",", fixed = TRUE)){
+    names <- coda::varnames(l)
+    names <- unique(unlist(strsplit(names, ",", TRUE))[seq(2, length(names) * 2, 2)])
+    ncol <- max(as.numeric(unlist(strsplit(names, "]", TRUE))))
+
+  }
+
+  ## calculate HPD interval
+  l <- coda::HPDinterval(l, prob = prob[1])
+
+  if(unlist) matrix(rowMeans(matrix(unlist(l), ncol = 2 * length(l))), ncol = ncol)
+
+}
